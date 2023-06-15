de7293ccb62fc972c02d8c705d601e0ea3858967
Sandhu Babu <53761189+SandhuBabu@users.noreply.github.com>
2023-06-15 23:39:55 +0530

    Update constants.js

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -14,6 +14,5 @@ export const LINKS = [
     { opt: 'Medical', path: '/category/Medical' },
     { opt: 'Education', path: '/category/Education' },
     { opt: 'Business', path: '/category/Business' },
-    { opt: 'Insurance', path: '/category/Insurance' },
     { opt: 'Others', path: '/category/Others' },
 ]
